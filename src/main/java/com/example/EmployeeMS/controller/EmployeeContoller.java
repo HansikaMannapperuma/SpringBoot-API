b4375412bbac79f53a4619085c8408836ce22053
Hansika Mannapperuma <hansikamannapperuma@gmail.com>
2023-03-07 17:42:57 +0530

    getAllEmpployees

--- src/main/java/com/example/EmployeeMS/controller/EmployeeContoller.java
+++ src/main/java/com/example/EmployeeMS/controller/EmployeeContoller.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/employee")
 public class EmployeeContoller {
@@ -86,6 +88,24 @@ public class EmployeeContoller {
 
         }
 
+    }
+
+    @GetMapping("/getAllEmployees")
+    public ResponseEntity getAllEmployee(){
+        try {
+            List<EmployeeDTO> employeeDTOList=employeeService.getAllEmployees();
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(employeeDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        }
+        catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
